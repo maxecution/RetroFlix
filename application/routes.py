2528df9de85e13eea955e2d9574283ecc0ead75d
LRsky9 <124300300+LRsky9@users.noreply.github.com>
2023-04-22 19:19:37 +0100

    updates
    account, search, back to top and sign up

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,22 @@
-from flask import render_template
-
+from flask import render_template, request, redirect, url_for
+#from flask_sqlalchemy import SQLAlchemy
 from application import app
+
+#!!!this code should import an external db and then store the data in it from the sign up page. It should then
+#import that data into the account page!!!
+
+#app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'<----DB link
+#db = SQLAlchemy(app)
+
+#class User(db.*insert_name_here*):
+    #id = db.Column(db.Integer, primary_key=True)
+    #email = db.Column(db.String(80), unique=True, nullable=False)
+    #password = db.Column(db.String(120), unique=True, nullable=False)
+    #first_name = db.Column(db.String(120), nullable=False)
+    #last_name = db.Column(db.String(120), nullable=False)
+    #dob = db.Column(db.String(120), nullable=False)
+
 
 #terms render
 @app.route('/terms_of_use')
@@ -63,14 +79,38 @@ def home():
     return render_template('home.html', title='Home')
 
 #account render
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 def account():
-    return render_template('account.html', title='Account')
+    if request.method == 'POST':
+        #user.email = request.form['email']
+        #user.password = request.form['password']
+        #user.first_name = request.form['first_name']
+        #user.last_name = request.form['last_name']
+        #user.dob = request.form['dob']
+        
+        return redirect(url_for('account'))
+    else:
+        return render_template('account.html', title='Account') #user=user
 
 #sign up render
-@app.route('/sign_up')
+@app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template('sign_up.html', title='Sign Up')
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+
+        #db.session.add(user)
+        #db.session.commit()
+
+# user defines the name of the chosen DB
+        #user = User(email=email, password=password, first_name=first_name, last_name=last_name, dob=dob)
+
+        return redirect(url_for('home')) #should this redirect to home so theyre logged in or is it to account as its defining where the information is sent to??
+    else:
+        return render_template('sign_up.html', title='Sign Up')
 
 #sign in render
 @app.route('/sign_in')
@@ -81,3 +121,22 @@ def sign_in():
 @app.route('/index')
 def index():
     return render_template('index.html', title='Index')
+
+#search bar render
+@app.route('/search')
+def search():
+    query = request.args.get('query')
+    
+    def perform_search(query):
+        pages = []
+        results = []
+        for page in pages:
+            if query.lower() in page.lower():
+                results.append(page)
+        if len(results) == 0:
+            return "No results found for '{}'".format(query)
+        else:
+            return results
+    
+    results = perform_search(query)
+    return render_template('search_results.html', results=[results])
