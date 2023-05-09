cf21f66d6d59b8c5f827a9202d1e583570cd4446
LRsky9 <124300300+LRsky9@users.noreply.github.com>
2023-05-09 13:29:02 +0100

    Update account.js

--- application/static/scripts/account.js
+++ application/static/scripts/account.js
@@ -1,6 +1,6 @@
 const passwordEls = document.querySelectorAll('#password, #pin, #card_details');
 passwordEls.forEach(passwordEl => {
-  passwordEl.innerText = '*'.repeat(8);
+  passwordEl.innerText = '*'.repeat(7);
 
 });
 
